6f786c7d2e2d9fd2cf2c1d004424194e79822807
aa <aa@163.com>
2022-03-01 14:40:41 +0800

    new

--- test.py
+++ test.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python
 print("hello")
-<<<<<<< HEAD
 print("hello")
-print("hello")
-print("hello")
-print("hello")
-=======
-print("ajhefjkls")
-print("aaaa666")
->>>>>>> 5b9ed1a1357e0c984164ed542dac892bc93cd93c
+print("newfile")
